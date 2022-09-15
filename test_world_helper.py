1ffccf56a4b8072bb6fcd9d4ee40899d27dfe4b1
Thiago Melo <thmelo@fb.com>
2022-09-15 10:18:07 +0100

    Introduce function to distribute populations

--- test_world_helper.py
+++ test_world_helper.py
@@ -1,9 +1,12 @@
 import pytest
 
-from world import World
-from world_helper import next_id
+from world_helper import distribute_populations
 
 
-def test_next_id():
-    world = World(initial_individuals=10)
-    assert next_id(world=world) == 10
+def test_distribute_populations():
+    points = distribute_populations(size=(100, 100), populations=10)
+    assert len(points) == 10
+    for p in points:
+        assert 0 <= p[0] < 100
+        assert 0 <= p[1] < 100
+    assert len(set(map(lambda p: f"{p[0]}-{p[1]}", points))) == 10
--- world_helper.py
+++ world_helper.py
@@ -1,5 +1,15 @@
-from world import World
+from random import randint
+from typing import List, Tuple
 
 
-def next_id(world: World) -> int:
-    return max([i.id for i in world.individuals]) + 1
+Point = Tuple[int, int]
+
+
+def distribute_populations(size: Tuple[int, int], populations: int) -> List[Point]:
+    points = []
+    for i in range(populations):
+        point = (randint(0, size[0] - 1), randint(0, size[1] - 1))
+        while point in points:
+            point = (randint(0, size[0] - 1), randint(0, size[1] - 1))
+        points.append(point)
+    return points
